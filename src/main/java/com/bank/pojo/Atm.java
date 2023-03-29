d4dc977ee3435ed4402e789aa843e3ccf1ff8e89
unknown <ramtekenishant2813@gmail.com>
2023-03-29 10:07:11 +0530

    added getter setter

--- src/main/java/com/bank/pojo/Atm.java
+++ src/main/java/com/bank/pojo/Atm.java
@@ -5,6 +5,24 @@ public class Atm {
 	private int id;
 	private String bankName;
 	private double balanace;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getBankName() {
+		return bankName;
+	}
+	public void setBankName(String bankName) {
+		this.bankName = bankName;
+	}
+	public double getBalanace() {
+		return balanace;
+	}
+	public void setBalanace(double balanace) {
+		this.balanace = balanace;
+	}
 	
 	
 
