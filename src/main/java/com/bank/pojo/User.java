ca7244fa225ed9b35f7776896ece18595b085df5
Ragini Thakrele <raginithakrele@gmail.com>
2023-03-29 23:16:24 -1100

    added getter setter and toString method

--- src/main/java/com/bank/pojo/User.java
+++ src/main/java/com/bank/pojo/User.java
@@ -13,4 +13,91 @@ public class User {
 	private String drivinglicence;
 	private String passport;
 
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmailId() {
+		return emailId;
+	}
+
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+
+	public long getMobileNumber() {
+		return mobileNumber;
+	}
+
+	public void setMobileNumber(long mobileNumber) {
+		this.mobileNumber = mobileNumber;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getAddharNumber() {
+		return addharNumber;
+	}
+
+	public void setAddharNumber(String addharNumber) {
+		this.addharNumber = addharNumber;
+	}
+
+	public String getPanNumber() {
+		return panNumber;
+	}
+
+	public void setPanNumber(String panNumber) {
+		this.panNumber = panNumber;
+	}
+
+	public String getDrivinglicence() {
+		return drivinglicence;
+	}
+
+	public void setDrivinglicence(String drivinglicence) {
+		this.drivinglicence = drivinglicence;
+	}
+
+	public String getPassport() {
+		return passport;
+	}
+
+	public void setPassport(String passport) {
+		this.passport = passport;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", emailId=" + emailId
+				+ ", mobileNumber=" + mobileNumber + ", password=" + password + ", addharNumber=" + addharNumber
+				+ ", panNumber=" + panNumber + ", drivinglicence=" + drivinglicence + ", passport=" + passport + "]";
+	}
+
 }
