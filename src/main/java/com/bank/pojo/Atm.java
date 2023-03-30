2d2d2a59e74058a2525dd0ed762727ae6f14e94f
unknown <ramtekenishant2813@gmail.com>
2023-03-30 09:29:29 +0530

    added a new member variables

--- src/main/java/com/bank/pojo/Atm.java
+++ src/main/java/com/bank/pojo/Atm.java
@@ -1,30 +1,16 @@
 package com.bank.pojo;
 
 public class Atm {
-	
+
 	private int id;
 	private String bankName;
 	private double balanace;
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getBankName() {
-		return bankName;
-	}
-	public void setBankName(String bankName) {
-		this.bankName = bankName;
-	}
-	public double getBalanace() {
-		return balanace;
-	}
-	public void setBalanace(double balanace) {
-		this.balanace = balanace;
-	}
-	 
-	
-	
+	private int pinNumber;
+	private String customerName;
+	private double deposite;
+	private double credit;
+	private int numberOfNotes;
+	private long contactNumber;
 
+	
 }
