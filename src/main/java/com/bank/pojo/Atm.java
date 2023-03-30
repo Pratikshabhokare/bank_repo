545b19150748e201611a159a9ebe1dd8d76bfb89
Pratiksha Bhokare <pratikshabhokare283@gmail.com>
2023-03-30 09:30:20 +0530

    deleted getter setter method and added new member variable in atm class

--- src/main/java/com/bank/pojo/Atm.java
+++ src/main/java/com/bank/pojo/Atm.java
@@ -5,24 +5,7 @@ public class Atm {
 	private int id;
 	private String bankName;
 	private double balanace;
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getBankName() {
-		return bankName;
-	}
-	public void setBankName(String bankName) {
-		this.bankName = bankName;
-	}
-	public double getBalanace() {
-		return balanace;
-	}
-	public void setBalanace(double balanace) {
-		this.balanace = balanace;
-	}
+	 private Address address;
 	 
 	
 	
