ad8a6021f1418dd54205860805866323c6b86ab7
Ragini Thakrele <raginithakrele@gmail.com>
2023-03-29 22:05:37 -1100

    generated getter and setter methods for Atm class

--- src/main/java/com/bank/pojo/Atm.java
+++ src/main/java/com/bank/pojo/Atm.java
@@ -12,5 +12,67 @@ public class Atm {
 	private int numberOfNotes;
 	private long contactNumber;
 	private Address address;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getBankName() {
+		return bankName;
+	}
+	public void setBankName(String bankName) {
+		this.bankName = bankName;
+	}
+	public double getBalanace() {
+		return balanace;
+	}
+	public void setBalanace(double balanace) {
+		this.balanace = balanace;
+	}
+	public int getPinNumber() {
+		return pinNumber;
+	}
+	public void setPinNumber(int pinNumber) {
+		this.pinNumber = pinNumber;
+	}
+	public String getCustomerName() {
+		return customerName;
+	}
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+	public double getDeposite() {
+		return deposite;
+	}
+	public void setDeposite(double deposite) {
+		this.deposite = deposite;
+	}
+	public double getCredit() {
+		return credit;
+	}
+	public void setCredit(double credit) {
+		this.credit = credit;
+	}
+	public int getNumberOfNotes() {
+		return numberOfNotes;
+	}
+	public void setNumberOfNotes(int numberOfNotes) {
+		this.numberOfNotes = numberOfNotes;
+	}
+	public long getContactNumber() {
+		return contactNumber;
+	}
+	public void setContactNumber(long contactNumber) {
+		this.contactNumber = contactNumber;
+	}
+	public Address getAddress() {
+		return address;
+	}
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+	
+	
 
 }
